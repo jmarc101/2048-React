5a4927a7ef8619c291dbbfe9542f408ef281ab4c
Jean-Marc Prud'homme <42589969+jmarc101@users.noreply.github.com>
2021-06-30 11:01:11 -0400

    logique des tuiles semi fini petit bug a reparer

--- src/Components/AppReducer.js
+++ src/Components/AppReducer.js
@@ -7,11 +7,11 @@ export const defaultState = {
 
 // REDUCER
 export const AppReducer = (state, action) => {
-
+  let newBoard = [];
   switch (action.type) {
     case "NEW_GAME":
       //new game
-      let newBoard = newGameBoard(state.numberOfRows);
+      newBoard = newGameBoard(state.numberOfRows);
       newBoard[getFirstTile(state.numberOfRows)] = 2;
       return { ...state, boardValues: newBoard, isFirstGame: false };
 
@@ -26,22 +26,27 @@ export const AppReducer = (state, action) => {
 
     case "MOVE_UP":
       //swipe up
-      playedUp();
-      return state;
+      newBoard = playedUp(state)
+      newBoard[getRandomIndexFromEmptySquares(newBoard)] = 2
+      return {...state, boardValues: newBoard}
       
 
     case "MOVE_RIGHT":
       //swipt right
-      playedRight();
-      return state;
+      newBoard = playedRight(state)
+      newBoard[getRandomIndexFromEmptySquares(newBoard)] = 2
+      return {...state, boardValues: newBoard}
+
 
     case "MOVE_DOWN":
-      playedDown();
-      return state;
+      newBoard = playedDown(state)
+      newBoard[getRandomIndexFromEmptySquares(newBoard)] = 2
+      return {...state, boardValues: newBoard}
 
     case "MOVE_LEFT":
-      playedLeft();
-      return state;
+      newBoard = playedLeft(state)
+      newBoard[getRandomIndexFromEmptySquares(newBoard)] = 2
+      return {...state, boardValues: newBoard}
 
     case "DEBUG":
       let board = [...state.boardValues];
@@ -75,23 +80,123 @@ const getRandomIndexFromEmptySquares = (board) => {
   return emptyTiles[Math.trunc(Math.random() * emptyTiles.length)];
 };
 
+const removeEmptyTiles = (array) => {
+  let nonEmptyTiles = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 0) nonEmptyTiles.push(array[i]);
+  }
+  return nonEmptyTiles;
+};
+
+const merge = (array) => {
+  let nonEmptyTiles = removeEmptyTiles(array);
+  var mergedRow = []; // sourceRow after it was merged
+
+  for (let i = 0; i < nonEmptyTiles.length; i++) {
+    // after all elements were pushed we push the last element because there is no element after to be merged with
+    if (i === nonEmptyTiles.length - 1) mergedRow.push(nonEmptyTiles[i]);
+    // comparing if values are mergeable
+    else if (nonEmptyTiles[i] === nonEmptyTiles[i + 1]) {
+      // elements got merged so a new element appears and gets incremented
+      // skip one element(i++) because it got merged
+      mergedRow.push(nonEmptyTiles[i] * 2);
+      i++;
+    } else {
+      // no merge, so follow normal order
+      mergedRow.push(nonEmptyTiles[i]);
+    }
+  }
+
+  return mergedRow;
+};
+
 
 
 /*  KEY PRESS LOGIC
  */
 // action when we press buttons
-const playedLeft = () => {
-  console.log("pressed left");
+
+const playedUp = (state) => {
+  let board = [...state.boardValues];
+  for (let row = 0; row < state.numberOfRows; row++) {
+    let indexes = [];
+    let arrayOfRow = [];
+    for (let i = 0; i < state.numberOfRows; i++) {
+      let index = i * state.numberOfRows + row;
+      indexes.push(index);
+      arrayOfRow.push(board[index]);
+    }
+
+    let rowArray = merge(arrayOfRow);
+    let arrayToConcat = new Array(state.numberOfRows - rowArray.length).fill(0);
+    const combine = [...rowArray, ...arrayToConcat];
+
+    for (let i = 0; i <= indexes.length; i++) {
+      board[indexes[i]] = combine[i];
+    }
+  }
+  return board;
 };
 
-const playedUp = () => {
-  console.log("pressed up");
+const playedRight = (state) => {
+  let board = [...state.boardValues];
+
+  for (let row = 0; row < state.numberOfRows; row++) {
+    let startIndex = row * state.numberOfRows;
+    let endIndex = row * state.numberOfRows + (state.numberOfRows - 1);
+    let rowArray = merge(board.slice(startIndex, endIndex + 1));
+
+    let arrayToConcat = new Array(state.numberOfRows - rowArray.length).fill(0);
+    const combine = [...arrayToConcat, ...rowArray];
+
+    let y = 0;
+    for (let i = startIndex; i <= endIndex; i++) {
+      board[i] = combine[y];
+      y++;
+    }
+  }
+  return board;
 };
 
-const playedRight = () => {
-  console.log("pressed right");
+const playedDown = (state) => {
+  let board = [...state.boardValues];
+  for (let row = 0; row < state.numberOfRows; row++) {
+    let indexes = [];
+    let arrayOfRow = [];
+    for (let i = 0; i < state.numberOfRows; i++) {
+      let index = i * state.numberOfRows + row;
+      indexes.push(index);
+      arrayOfRow.push(board[index]);
+    }
+
+    let rowArray = merge(arrayOfRow);
+    let arrayToConcat = new Array(state.numberOfRows - rowArray.length).fill(0);
+    const combine = [...arrayToConcat, ...rowArray];
+
+    for (let i = 0; i <= indexes.length; i++) {
+      board[indexes[i]] = combine[i];
+    }
+  }
+  return board;
 };
 
-const playedDown = () => {
-  console.log("pressed down");
+const playedLeft = (state) => {
+  let board = [...state.boardValues];
+
+  for (let row = 0; row < state.numberOfRows; row++) {
+    let startIndex = row * state.numberOfRows;
+    let endIndex = row * state.numberOfRows + (state.numberOfRows - 1);
+    let rowArray = merge(board.slice(startIndex, endIndex + 1));
+
+    let arrayToConcat = new Array(state.numberOfRows - rowArray.length).fill(0);
+    const combine = [...rowArray, ...arrayToConcat];
+
+    let y = 0;
+    for (let i = startIndex; i <= endIndex; i++) {
+      board[i] = combine[y];
+      y++;
+    }
+  }
+  return board;
 };
+
